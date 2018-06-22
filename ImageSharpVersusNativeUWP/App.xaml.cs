56fadf48401893a91c74db58b772c5e84ec8d767
Marcos Cobeña Morián <mcobena@plainconcepts.com>
2018-06-22 10:56:20 +0200

    Added initial UWP runner

---
+++ ImageSharpVersusNativeUWP/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using Xunit.Runners.UI;
+
+namespace UwpTestRunner
+{
+    sealed partial class App : RunnerApplication
+    {
+        protected override void OnInitializeRunner()
+        {
+            AddTestAssembly(GetType().GetTypeInfo().Assembly);
+        }
+    }
+}
